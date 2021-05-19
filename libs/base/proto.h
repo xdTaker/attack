2ff264a5cf2bfc6111c5f1a1d4576712067149bf
Eunha <duang.eunha@foxmail.com>
2021-05-19 13:59:33 +0800

    增加全编译脚本；修改字段名称；

--- libs/base/proto.h
+++ libs/base/proto.h
@@ -2,7 +2,7 @@
 
 #include "debug.h"
 
-enum TRANS_PROTO{
+enum TRANS_PROTO {
     TRANS_ICMP = 1,
     TRANS_IGMP = 2,
     TRANS_TCP = 6,
@@ -10,6 +10,22 @@ enum TRANS_PROTO{
     TRANS_OSPF = 89,
 };
 
+enum Port_for_Proto {
+    Port_FtpData = 20,
+    Port_Ftp = 21,
+    Port_Ssh = 22,
+    Port_Telent = 23,
+    Port_Smtp = 25,
+    Port_Dns = 53,
+    Port_Tftp = 69,
+    Port_Http = 80,
+    Port_Pop2 = 109,
+    Port_Pop3 = 110,
+
+    Port_Https = 443,
+
+    Port_Mysql = 3306,
+};
 
 enum Layer {
     Layer_TOP,
